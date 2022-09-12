a70808d51fae5e39b667e165bdb741c7c0d82144
Yi Zhou <ghzhouyi@gmail.com>
2022-09-12 11:08:32 -0700

    updated some comments in solver.m

--- Solver.m
+++ Solver.m
@@ -1,9 +1,9 @@
 %define grid points
 p=8;
 h=2*pi/2^p;
-ld=1/2;%try 1/128 will take >an hour 
+ld=1/2;
 dt=h*ld;
-z=(-pi+h):h:pi;%grid points excluding z=0
+z=(-pi+h):h:pi;%excluding z=-pi (periodic boundary condition)
 tf=150;
 t=0:dt:tf;
 N=length(z);
@@ -14,19 +14,19 @@ mi=70000;
 tauRT=100;
 kbar=2*pi;
 I=0.8;
-A0=1;
+A0=1;%initial average area
 Te0=0.8;
 %Te=Te0*(1+0*exp(T/30));%constant
 Te=Te0*exp(T/50);%increasing temperature
-n0=0.8;
+n0=0.8;%initial constant density
 Rei0=-30;
 epsilon=0.05;%amplitude
 A=A0*(1-epsilon*exp(T/tauRT).*cos(kbar*Z));%harmonic area perturbation
 ue0=I/(n0*A0*(1-epsilon));%initial electron velocity at choked region
-gamma=kbar*ue0*sqrt(1/mi)*sqrt(1-Te0/ue0^2);%growth rate of instability
+gamma=kbar*ue0*sqrt(1/mi)*sqrt(1-Te0/ue0^2);%growth rate of instability at choked region
 tau_gamma=1/gamma;%initial growth time of instability
 %resistive electric field
-Er=-Rei0*A0./A.*(Te0^(3/2)./Te.^(3/2));
+Er=-Rei0*(A0./A).*(Te0^(3/2)./Te.^(3/2));
 density=ones(M,N);%to store numerical solution for volume density (first index is time, second index is space)
 E_field=zeros(M,N);%to store numerical solution for electric field
 u_e=zeros(M,N);%to store numerical solution for electron velocity
@@ -34,8 +34,8 @@ u_ion=zeros(M,N);%to store numerical solution for ion velocity
 %%initial condition for u_ion and density%%
 density_0=n0*ones(1,N);
 u_ion_0=zeros(1,N);
-density(1,:)=density_0;%initial state of density
-u_ion(1,:)=u_ion_0;%initial state of ion velocity
+density(1,:)=density_0;
+u_ion(1,:)=u_ion_0;
 u_e(1,:)=-I./(density(1,:).*A(1,:));
 E_field(1,1)=Er(1,1)-(u_e(1,2)^2-u_e(1,N)^2)/(4*h)-Te(1,1)/(2*h)*(log(density(1,2))-log(density(1,N)));
 E_field(1,2:N-1)=Er(1,2:N-1)-(u_e(1,3:N).^2-u_e(1,1:N-2).^2)/(4*h)-Te(1,2:N-1)/(2*h).*(log(density(1,3:N))-log(density(1,1:N-2)));
