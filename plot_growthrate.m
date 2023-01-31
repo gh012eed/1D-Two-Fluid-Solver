8573db2510b918cb11a9b051e06a792030bd46e5
Yi Zhou <ghzhouyi@gmail.com>
2023-01-30 16:59:27 -0800

    updated plot_growthrate to plot theory growth

--- plot_growthrate.m
+++ plot_growthrate.m
@@ -1,4 +1,6 @@
 %plot growth rate
+
+
 z_start=round((-2*pi/(3*kbar)+pi)/h); 
 z_end=round((2*pi/(3*kbar)+pi)/h);
 reduced_ue= -1*u_e(:,z_start:z_end);
@@ -6,12 +8,22 @@ max_ue=max(reduced_ue,[],2);
 due=ue0-u0;
 x_data=T(:,1);
 y_data=abs(max_ue-u0);
-f=fit(x_data,y_data,'exp1')
-plot(u_e(1,z_start:z_end))
 fs=30
-figure
-plot(f,x_data,y_data)
 
+kz=628;
+u=uref*ue0;
+me=9.1e-31;
+mi=70000*me;
+vte=5.9e5;
+theory=kz*sqrt(me/mi*(u*u-vte*vte));
+theory_ref=theory*tref;%growth rate in reference units
+
+figure
+%f=fit(x_data,y_data,'exp1') % exponential fit
+%plot(f,x_data,y_data)
+plot(x_data,y_data,'linewidth',2)
+hold on
+plot(x_data,y_data(1)*exp(theory_ref*x_data),'linewidth',2)
 l=6;
 w=6*2.5;
 fig=gcf;
@@ -22,4 +34,4 @@ ax.FontSize = fs;
 ylabel('$\Delta \bar{u}_e$','Interpreter','latex','fontsize',fs+18)
 xlabel('$\bar{t}$','Interpreter','latex','fontsize',fs+18)
 ylim([1,5])
-legend("data","fit",'fontsize',fs)
+legend("Numerical data","Linear theory",'fontsize',fs)
--- plot_growthrate_density.m
+++ plot_growthrate_density.m
@@ -5,10 +5,22 @@ reduced_n= density(:,z_start:z_end);
 min_n=min(reduced_n,[],2);%min density as a function of time
 x_data=T(:,1);
 y_data=abs(min_n-n0);
-f=fit(x_data,y_data,'exp1')
 fs=30
+
+kz=628;
+u=uref*ue0;
+me=9.1e-31;
+mi=70000*me;
+vte=5.9e5;
+theory=kz*sqrt(me/mi*(u*u-vte*vte));
+theory_ref=theory*tref;%growth rate in reference units
+
 figure
-plot(f,x_data,y_data)
+
+plot(x_data,y_data,'linewidth',2)
+hold on
+plot(x_data,y_data(1)*exp(theory_ref*x_data),'linewidth',2)
+
 l=6;
 w=6*2.5;
 fig=gcf;
